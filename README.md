2605b936b74f78ad88260a95338f1d6bf11f6ac2
ahma1939 <124739585+ahma1939@users.noreply.github.com>
2024-06-15 02:11:01 +0400

    done

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# Проектная работа Mesto
+# Проект: Место
 
+Ссылка:
+https://ahma1939.github.io/mesto-project-ff/
+
+### Обзор
+* Интро
+* Figma
+* Картинки
+
+## Технологии: 
+
+  * HTML;
+  * CSS;
+  * JScript;
+  * DOM;
+  * Figma;
+  * Git;
+  * GitHub Pages;
+  * BEM;
+  * Grid layout;
+  * Flexbox;
+
+
+**Интро**
+Прект сделан по макету Figma.
+Применем JScript, так же адаптирован с помощью @import под разрешения: 1280px, 320px. Добавлен редактор профиля.
+
+**Figma**
+
+* [Ссылка на макет в Figma]https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1
+
+**Картинки**
+
+<img src="./images/Add Button.svg" alt="">
+<img src="./images/Close Button.svg" alt="">
+<img src="./images/Like Button Active.svg" alt="">
+<img src="./images/Like Button.svg" alt="">
+<img src="./images/logo.svg" alt="">
+<img src="./images/Edit Button.svg" alt="">
+<img src="./images/Elbrus.jpg" alt="">
+<img src="./images/Karachaevsk.jpg" alt="">
+<img src="./images/Kusto.jpg" alt="">
+<img src="./images/Dombai.jpg" alt="">
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,64 @@
-// @todo: Темплейт карточки
+//Profile
+const popupEditProfile = document.querySelector('.popup_edit-profile');
+const buttonEditProfile = document.querySelector('.profile__edit-button');
+const nameInput = document.querySelector('.popup__input_type_name');
+const descriptionInput = document.querySelector('.popup__input_type_description');
+const profileName = document.querySelector('.profile__name');
+const profileDescription = document.querySelector('.profile__description');
+const submitEditProfilePopup = document.querySelector('.popup__form');
 
-// @todo: DOM узлы
+//Add Card
+const popupAddCard = document.querySelector('.popup_add-card');
+const buttonAddCard = document.querySelector('.profile__add-button');
 
-// @todo: Функция создания карточки
+//Close Button
+const popupEditProfilCloseButton = popupEditProfile.querySelector('.popup__close-button');
+const popupAddCardCloseButton = popupAddCard.querySelector('.popup__close-button');
+const buttonClosePopup = popupEditProfile.querySelector('.popup__close-button');
 
-// @todo: Функция удаления карточки
+//Open Popup
+const openPopup = (popup) => {
+    popup.classList.add('popup_opened');
+};
 
-// @todo: Вывести карточки на страницу
+//Close Popup
+const closePopup = (popup) => {
+    popup.classList.remove('popup_opened');
+};
+
+popupEditProfilCloseButton.addEventListener('click', () => {
+    closePopup(popupEditProfile);
+});
+
+popupAddCardCloseButton.addEventListener('click', () => {
+    closePopup(popupAddCard);
+});
+
+
+
+
+buttonAddCard.addEventListener('click', function () {
+    popupAddCard.classList.add('popup_opened');
+});
+
+//Fuction Edit Profile
+buttonEditProfile.addEventListener('click', function () {
+    nameInput.value = profileName.textContent;
+    descriptionInput.value = profileDescription.textContent;
+    openPopup(popupEditProfile);
+});
+
+buttonClosePopup.addEventListener('click', function () {
+    closePopup(popupEditProfile);
+});
+
+function handleclick(event) {
+    event.preventDefault();
+
+    profileName.textContent = nameInput.value;
+    profileDescription.textContent = descriptionInput.value;
+
+    closePopup(popupEditProfile);
+}
+
+submitEditProfilePopup.addEventListener('submit', handleclick);
